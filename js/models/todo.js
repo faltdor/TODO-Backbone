582e6ae4430e182a68d34820d72453880dde5850
faltdor <faltdor@gmail.com>
2015-08-02 18:07:34 -0500

    initial commit Sample Double-click to edit a todo Sample Ebook Developing Backbone.js Applications

---
+++ js/models/todo.js
@@ -0,0 +1,17 @@
+var app = app || {};
+
+// Todo Model
+// ----------
+// Our basic **Todo** model has 'title', 'order', and 'completed' attributes.
+
+app.Todo = Backbone.Model.extend({
+	defaults:{
+		title:'',
+		completed:false
+	},
+	toggle: function(){
+		this.save({
+			completed : !this.get('completed')
+		});
+	}
+});
